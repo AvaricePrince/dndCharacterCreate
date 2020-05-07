855d18f8959fdd617da4d125d764109d0aecd872
Raul Mendez <raulmendez@Rauls-MBP.fios-router.home>
2020-05-06 22:43:29 -0400

    adding so much

--- DNDCharacter.py
+++ DNDCharacter.py
@@ -4,9 +4,19 @@ import linecache as line
 
 _Character = {"pname":"", "fname":"", "lname":"","level":"", "race":"", "class":"", "alignment":""}
 Items = {}
-abilityScore = {"Strength":"", "Dexterity":"", "Constitution":"", "Intelligence":"", "Wisdom":"", "Charisma":"", "**PROFICIENCY BONUS**":""}
+abilityScore = {"Strength":(5,"Modifier: ",-5), "Dexterity":"", "Constitution":"", "Intelligence":"", "Wisdom":"", "Charisma":"", "**PROFICIENCY BONUS**":2}
 savingThrows = {"Saving Strength":"", "Saving Dexterity":"", "Saving Constitution":"", "Saving Intelligence":"", "Saving Wisdom":"", "Saving Charisma":""}
+# classSaves = {"barbarian":("str","con"), "bard":("dex","cha"), "cleric":("wis","cha")}
+#-----------GLOBAL VARIABLES--------#
+save = "s","c"
+sMod = -5
+dMod = ""
+cMod = ""
+iMod = ""
 
+#-----------GLOBAL VARIABLES--------#
+
+#==========MAIN FUNCTION==========#
 def _MakeACharacter():
     _Logo()
     while True:
@@ -42,6 +52,8 @@ def _MakeACharacter():
         else:
             print("Try again")
             continue
+
+#==========FUNCTIONS==========#
 
 def lookUpCharacter():
     while True:
@@ -309,10 +321,10 @@ def updateRace():
                 print("You are a Grung\n")
                 break
             else:
-                print("No race selected")
+                print("No race selected\n")
                 continue
         except ValueError:
-            print("Invalid, must be a number")
+            print("Invalid, must be a number\n")
             continue
 
 def updateClass():
@@ -340,42 +352,59 @@ def updateClass():
             if choice == 1:
                 _Character["class"] = "Barbarian"
                 print("You are a Barbarian\n")
+                save = "s","c"
+                break
             elif choice == 2:
                 _Character["class"] = "Bard"
                 print("You are a Bard\n")
+                break
             elif choice == 3:
                 _Character["class"] = "Cleric"
                 print("You are a Cleric\n")
+                break
             elif choice == 4:
                 _Character["class"] = "Druid"
                 print("You are a Druid\n")
+                break
             elif choice == 5:
                 _Character["class"] = "Fighter"
                 print("You are a Fighter\n")
+                break
             elif choice == 6:
                 _Character["class"] = "Monk"
                 print("You are a Monk\n")
+                break
             elif choice == 7:
                 _Character["class"] = "Paladin"
                 print("You are a Paladin\n")
+                break
             elif choice == 8:
                 _Character["class"] = "Ranger"
                 print("You are a Ranger\n")
+                break
             elif choice == 9:
                 _Character["class"] = "Rogue"
                 print("You are a Rogue\n")
+                break
             elif choice == 0:
                 _Character["class"] = "Sorcerer"
                 print("You are a Sorcerer\n")
+                break
             elif choice == 11:
                 _Character["class"] = "Warlock"
                 print("You are a Warlock\n")
+                break
             elif choice == 12:
                 _Character["class"] = "Wizard"
                 print("You are a Wizard\n")
+                break
             elif choice == 13:
                 _Character["class"] = "Artificer"
                 print("You are a Artificer\n")
+                break
+            else:
+                print("Class does not exist\n")
+                continue
 
         except ValueError:
             print("Must be a number!\n")
@@ -474,54 +503,55 @@ def updateAbilityScore():
         try:
             st = int(input("What did you roll for strength? "))
             if st < 2:
-                abilityScore["Strength"] = (st, "Modifier: -5")
+                abilityScore["Strength"] = (st, "Modifier: ", -5)
+                modifier = -5
             elif st <= 3: 
-                abilityScore["Strength"] = (st, "Modifier: -4")
+                abilityScore["Strength"] = (st, "Modifier: ", -4)
             elif st <= 5:
-                abilityScore["Strength"] = (st, "Modifer: -3")
+                abilityScore["Strength"] = (st, "Modifer: ", -3)
             elif st <= 7:
-                abilityScore["Strength"] = (st, "Modifier: -2")
+                abilityScore["Strength"] = (st, "Modifier: ", -2)
             elif st <= 9:
-                abilityScore["Strength"] = (st, "Modifier: -1")
+                abilityScore["Strength"] = (st, "Modifier: ", -1)
             elif st <= 11:
-                abilityScore["Strength"] = (st, "Modifier: 0")
+                abilityScore["Strength"] = (st, "Modifier: ", 0)
             elif st <= 13:
-                abilityScore["Strength"] = (st, "Modifier: +1")
+                abilityScore["Strength"] = (st, "Modifier: ", 1)
             elif st <= 15:
-                abilityScore["Strength"] = (st, "Modifier: +2")
+                abilityScore["Strength"] = (st, "Modifier: ", 2)
             elif st <= 17:
-                abilityScore["Strength"] = (st, "Modifier: +3")
+                abilityScore["Strength"] = (st, "Modifier: ", 3)
             else:
                 print("Invalid entry")
                 continue
 
             dex = int(input("What did you roll for Dexterity? "))
             if dex < 2:
-                abilityScore["Dexterity"] = (dex, "Modifier: -5")
+                abilityScore["Dexterity"] = (dex, "Modifier: ", -5)
 
             elif dex <= 3: 
-                abilityScore["Dexterity"] = (dex, "Modifier: -4")
+                abilityScore["Dexterity"] = (dex, "Modifier: ", -4)
 
             elif dex <= 5:
-                abilityScore["Dexterity"] = (dex, "Modifer: -3")
+                abilityScore["Dexterity"] = (dex, "Modifer: ", -3)
 
             elif dex <= 7:
-                abilityScore["Dexterity"] = (dex, "Modifier: -2")
+                abilityScore["Dexterity"] = (dex, "Modifier: ", -2)
 
             elif dex <= 9:
-                abilityScore["Dexterity"] = (dex, "Modifier: -1")
+                abilityScore["Dexterity"] = (dex, "Modifier: ", -1)
 
             elif dex <= 11:
-                abilityScore["Dexterity"] = (dex, "Modifier: 0")
+                abilityScore["Dexterity"] = (dex, "Modifier: ", 0)
  
             elif dex <= 13:
-                abilityScore["Dexterity"] = (dex, "Modifier: +1")
+                abilityScore["Dexterity"] = (dex, "Modifier: ", 1)
 
             elif dex <= 15:
-                abilityScore["Dexterity"] = (dex, "Modifier: +2")
+                abilityScore["Dexterity"] = (dex, "Modifier: ", 2)
 
             elif dex <= 17:
-                abilityScore["Dexterity"] = (dex, "Modifier: +3")
+                abilityScore["Dexterity"] = (dex, "Modifier: ", 3)
 
             else:
                 print("Invalid entry")
@@ -529,31 +559,31 @@ def updateAbilityScore():
 
             con = int(input("What did you roll for Constitution? "))
             if con < 2:
-                abilityScore["Constitution"] = (con, "Modifier: -5")
+                abilityScore["Constitution"] = (con, "Modifier: ",-5)
 
             elif con <= 3: 
-                abilityScore["Constitution"] = (con, "Modifier: -4")
+                abilityScore["Constitution"] = (con, "Modifier: ",-4)
 
             elif con <= 5:
-                abilityScore["Constitution"] = (con, "Modifer: -3")
+                abilityScore["Constitution"] = (con, "Modifer: ",-3)
 
             elif con <= 7:
-                abilityScore["Constitution"] = (con, "Modifier: -2")
+                abilityScore["Constitution"] = (con, "Modifier: ",-2)
 
             elif con <= 9:
-                abilityScore["Constitution"] = (con, "Modifier: -1")
+                abilityScore["Constitution"] = (con, "Modifier: ",-1)
 
             elif con <= 11:
-                abilityScore["Constitution"] = (con, "Modifier: 0")
+                abilityScore["Constitution"] = (con, "Modifier: ",0)
               
             elif con <= 13:
-                abilityScore["Constitution"] = (con, "Modifier: +1")
+                abilityScore["Constitution"] = (con, "Modifier: ",1)
                
             elif con <= 15:
-                abilityScore["Constitution"] = (con, "Modifier: +2")
+                abilityScore["Constitution"] = (con, "Modifier: ",2)
               
             elif con <= 17:
-                abilityScore["Constitution"] = (con, "Modifier: +3")
+                abilityScore["Constitution"] = (con, "Modifier: ",3)
                
             else:
                 print("Invalid entry")
@@ -561,31 +591,31 @@ def updateAbilityScore():
 
             intel = int(input("What did you roll for Intelligence? "))
             if intel < 2:
-                abilityScore["Intelligence"] = (intel, "Modifier: -5")
+                abilityScore["Intelligence"] = (intel, "Modifier: ",-5)
                
             elif intel <= 3: 
-                abilityScore["Intelligence"] = (intel, "Modifier: -4")
+                abilityScore["Intelligence"] = (intel, "Modifier: ",-4)
            
             elif intel <= 5:
-                abilityScore["Intelligence"] = (intel, "Modifer: -3")
+                abilityScore["Intelligence"] = (intel, "Modifer: ",-3)
                 
             elif intel <= 7:
-                abilityScore["Intelligence"] = (intel, "Modifier: -2")
+                abilityScore["Intelligence"] = (intel, "Modifier: ",-2)
                
             elif intel <= 9:
-                abilityScore["Intelligence"] = (intel, "Modifier: -1")
+                abilityScore["Intelligence"] = (intel, "Modifier: ",-1)
              
             elif intel <= 11:
-                abilityScore["Intelligence"] = (intel, "Modifier: 0")
+                abilityScore["Intelligence"] = (intel, "Modifier: ",0)
              
             elif intel <= 13:
-                abilityScore["Intelligence"] = (intel, "Modifier: +1")
+                abilityScore["Intelligence"] = (intel, "Modifier: ",1)
        
             elif intel <= 15:
-                abilityScore["Intelligence"] = (intel, "Modifier: +2")
+                abilityScore["Intelligence"] = (intel, "Modifier: ",2)
               
             elif intel <= 17:
-                abilityScore["Intelligence"] = (intel, "Modifier: +3")
+                abilityScore["Intelligence"] = (intel, "Modifier: ",3)
     
             else:
                 print("Invalid entry")
@@ -593,31 +623,31 @@ def updateAbilityScore():
 
             wis = int(input("What did you roll for Wisdom? "))
             if wis < 2:
-                abilityScore["Wisdom"] = (wis, "Modifier: -5")
+                abilityScore["Wisdom"] = (wis, "Modifier: ",-5)
         
             elif wis <= 3: 
-                abilityScore["Wisdom"] = (wis, "Modifier: -4")
+                abilityScore["Wisdom"] = (wis, "Modifier: ",-4)
              
             elif wis <= 5:
-                abilityScore["Wisdom"] = (wis, "Modifer: -3")
+                abilityScore["Wisdom"] = (wis, "Modifer: ",-3)
              
             elif wis <= 7:
-                abilityScore["Wisdom"] = (wis, "Modifier: -2")
+                abilityScore["Wisdom"] = (wis, "Modifier: ",-2)
               
             elif wis <= 9:
-                abilityScore["Wisdom"] = (wis, "Modifier: -1")
+                abilityScore["Wisdom"] = (wis, "Modifier: ",-1)
         
             elif wis <= 11:
-                abilityScore["Wisdom"] = (wis, "Modifier: 0")
+                abilityScore["Wisdom"] = (wis, "Modifier: ",0)
         
             elif wis <= 13:
-                abilityScore["Wisdom"] = (wis, "Modifier: +1")
+                abilityScore["Wisdom"] = (wis, "Modifier: ",1)
          
             elif wis <= 15:
-                abilityScore["Wisdom"] = (wis, "Modifier: +2")
+                abilityScore["Wisdom"] = (wis, "Modifier: ",2)
                
             elif wis <= 17:
-                abilityScore["Wisdom"] = (wis, "Modifier: +3")
+                abilityScore["Wisdom"] = (wis, "Modifier: ",3)
            
             else:
                 print("Invalid entry")
@@ -625,31 +655,31 @@ def updateAbilityScore():
 
             cha = int(input("What did you roll for Charisma? "))
             if cha < 2:
-                abilityScore["Charisma"] = (cha, "Modifier: -5")
+                abilityScore["Charisma"] = (cha, "Modifier: ",-5)
               
             elif cha <= 3: 
-                abilityScore["Charisma"] = (cha, "Modifier: -4")
+                abilityScore["Charisma"] = (cha, "Modifier: ",-4)
               
             elif cha <= 5:
-                abilityScore["Charisma"] = (cha, "Modifer: -3")
+                abilityScore["Charisma"] = (cha, "Modifer: ",-3)
              
             elif cha <= 7:
-                abilityScore["Charisma"] = (cha, "Modifier: -2")
+                abilityScore["Charisma"] = (cha, "Modifier: ",-2)
                 
             elif cha <= 9:
-                abilityScore["Charisma"] = (cha, "Modifier: -1")
+                abilityScore["Charisma"] = (cha, "Modifier: ",-1)
                
             elif cha <= 11:
-                abilityScore["Charisma"] = (cha, "Modifier: 0")
+                abilityScore["Charisma"] = (cha, "Modifier: ",0)
                 
             elif cha <= 13:
-                abilityScore["Charisma"] = (cha, "Modifier: +1")
+                abilityScore["Charisma"] = (cha, "Modifier: ",1)
                 
             elif cha <= 15:
-                abilityScore["Charisma"] = (cha, "Modifier: +2")
+                abilityScore["Charisma"] = (cha, "Modifier: ",2)
                 
             elif cha <= 17:
-                abilityScore["Charisma"] = (cha, "Modifier: +3")
+                abilityScore["Charisma"] = (cha, "Modifier: ",3)
                
             else:
                 print("Invalid entry")
@@ -715,15 +745,15 @@ def clearAllCharacters():
 #calculates the proficiency bonus based on inputed level
 def proBo(level = None):
     for i in range(1,4):
-        abilityScore["**PROFICIENCY BONUS**"] = ("+2")
+        abilityScore["**PROFICIENCY BONUS**"] = 2
     for i in range(5,9):
-        abilityScore["**PROFICIENCY BONUS**"] = ("+3")
+        abilityScore["**PROFICIENCY BONUS**"] = 3
     for i in range(9,13):
-        abilityScore["**PROFICIENCY BONUS**"] = ("+4")
+        abilityScore["**PROFICIENCY BONUS**"] = 4
     for i in range(13,17):
-        abilityScore["**PROFICIENCY BONUS**"] = ("+5")
+        abilityScore["**PROFICIENCY BONUS**"] = 5
     for i in range(17,21):
-        abilityScore["**PROFICIENCY BONUS**"] = ("+6")
+        abilityScore["**PROFICIENCY BONUS**"] = 6
 
 def _Logo():
     print("""
@@ -747,5 +777,13 @@ def _Logo():
      ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝               
     """)
 
-# _MakeACharacter()
+# def calcSaveThrow():
+#     savingThrows["Saving Strength"] = modifier
+#     savingThrows["Saving Dexterity"] 
+#     savingThrows["Saving Constitution"]
+#     savingThrows["Saving Intelligence"]
+#     savingThrows["Saving Wisdom"]
+#     savingThrows["Saving Charisma"]
+
+_MakeACharacter()
 
